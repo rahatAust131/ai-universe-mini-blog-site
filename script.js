c4e45f6ac9c9b02ae2e0afdec731cc7585bc4ac2
Rahat Chowdhury <rczisan@gmail.com>
2023-10-07 17:27:29 +0600

    tried to add functionality of Sort By Date button

--- script.js
+++ script.js
@@ -1,35 +1,42 @@
 
-const someDataContainerDiv = document.getElementById("some-data-container");
-const allDataContainerDiv = document.getElementById("all-data-container");
+const dataContainerDiv = document.getElementById("data-container");
 const seeMoreBtn = document.getElementById("see-more-btn");
 const loaderDiv = document.getElementById("loader");
+const sortByDateBtn = document.getElementById("sort-by-date-btn");
+
+sortByDateBtn.style.display = "none";
+seeMoreBtn.style.display = "none";
 
 const loadData = () => {
   fetch("https://openapi.programming-hero.com/api/ai/tools")
-  .then(res => res.json())
-  .then(data => displaySomeData(data.data.tools))
+    .then(res => res.json())
+    .then(data => passDataInAllFunction(data.data.tools))
 };
 
 loadData();
 
+const passDataInAllFunction = (dataArr) => {
+  const newDataArr = [...dataArr];
+  displaySomeData(newDataArr);
+  sortByDateBtn.style.display = "block";
+  seeMoreBtn.style.display = "block";
+};
 
-const displayBlogDetails = (id) => {
-  console.log(id, "Blog details in console!")
-}
+// const displayBlogDetails = (id) => {
+//   console.log(id, "Blog details in console!")
+// }
 
 
 const displaySomeData = (results) => {
   loaderDiv.style.display = "none";
-  // allDataContainerDiv.style.display = "none";
+  // dataContainerDiv.style.display = "none";
 
-  const arr = [...results];
-  const filteredResultsArr = arr.filter(result => {
-    return +result.id %2 != 0; 
-  })
+  const filteredResultsArr = results.filter(result => +result.id % 2 != 0);
+
   filteredResultsArr.forEach(filteredResult => {
     const cardsDiv = document.createElement("div")
-    cardsDiv.innerHTML = 
-    `
+    cardsDiv.innerHTML =
+      `
     <div class="card">
       <div class="card-top">
         <img class="card-img" src=${filteredResult.name == "Jasper Chat" || filteredResult.name == "Replika" ? "https://www.simplilearn.com/ice9/free_resources_article_thumb/Types_of_Artificial_Intelligence.jpg" : filteredResult.image} alt="${filteredResult.name} image couldn't load">
@@ -53,29 +60,42 @@ const displaySomeData = (results) => {
       </div>
       </div>
     `;
-    someDataContainerDiv.appendChild(cardsDiv);
+    dataContainerDiv.appendChild(cardsDiv);
   })
-  
+
   seeMoreBtn.addEventListener('click', () => {
-    someDataContainerDiv.style.display = "none";
-    displayAllData(results); 
+    dataContainerDiv.innerHTML = '';
+    displayAllData(results);
   });
+
+  sortByDateBtn.addEventListener('click', () => {
+    const newResults = [...results, isSorted = false]
+    sortDataOnClick(results);
+  })
+
 };
 
-// const sortDataOnClick = () => {
-//   console.log("data");
-// }
-
-// sortDataOnClick();
+const sortDataOnClick = (data) => {
+  dataContainerDiv.innerHTML = "";
+  if(data) {
+    data.sort((a, b) => a.published_in - b.published_in);
+    data.isSorted = true;
+  }
+  if(data.isSorted) {
+    displayAllData(data)
+  } else {
+    if(alert("already sorted!")) {
+      displaySomeData(data);
+    }
+  }
+}
 
 const displayAllData = (dataArr) => {
-  // console.log("hi", dataArr);
-
   const newDataArr = [...dataArr]
   newDataArr.map(newData => {
     const cardsDiv = document.createElement("div");
-    cardsDiv.innerHTML = 
-    `
+    cardsDiv.innerHTML =
+      `
     <div class="card">
       <div class="card-top">
       <img class="card-img" src=${newData.name == "Jasper Chat" || newData.name == "Replika" ? "https://www.simplilearn.com/ice9/free_resources_article_thumb/Types_of_Artificial_Intelligence.jpg" : newData.image} alt="${newData.name} image couldn't load">
@@ -92,14 +112,15 @@ const displayAllData = (dataArr) => {
       <div class="card-bottom">
         <div class="left">
           <h3 class="card-title">${newData.name}</h3>
-          <p class="card-publish-date"><i class="fa-regular fa-calendar"></i> ${newData.published_in}</p></div>
+          <p class="card-publish-date"><i class="fa-regular fa-calendar"></i> ${newData.published_in}</p>
+        </div>
         <div onclick="displayBlogDetails(${newData.id})" class="right">
           <i class="fa-solid fa-arrow-right"></i>
         </div>
       </div>
-      </div>
+    </div>
     `;
-    allDataContainerDiv.appendChild(cardsDiv);
+    dataContainerDiv.appendChild(cardsDiv);
   })
   seeMoreBtn.style.display = "none";
 }
